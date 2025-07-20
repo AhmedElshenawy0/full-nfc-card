78bc5798c85c47deb0cd3a03bdc1ab42edc8826e
AhmedElshenawy0 <shenawy100@gmail.com>
2025-07-20 16:37:47 +0200

    test

--- frontend/src/pages/SelectService.tsx
+++ frontend/src/pages/SelectService.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import logo from "/images/SignUp Logo White (1).png";
+// import logo from "/images/SignUp Logo White (1).png";
 import Typewriter from "typewriter-effect";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
