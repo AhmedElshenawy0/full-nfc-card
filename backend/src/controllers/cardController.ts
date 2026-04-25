5e94f2bb2d3f5ea529ca6d8938a6f09a995bc0e8
AhmedElshenawy0 <shenawy100@gmail.com>
2026-04-25 15:01:44 +0300

    fix deactivate issue

--- backend/src/controllers/cardController.ts
+++ backend/src/controllers/cardController.ts
@@ -188,9 +188,7 @@ export const deactivateCard = async (
     await prisma.soldService.deleteMany({
       where: { card_id: card?.id },
     });
-    await prisma?.client?.delete({
-      where: { id: card?.client_id as string },
-    });
+
     await prisma.card.update({
       where: { unique_code },
       data: {
